e022020324fde3bc397481489cd468abe41d5c22
Ramesh <ramesh@rsageventures.com>
2018-10-02 11:40:21 +0530

    second commit

--- src/main/java/com/rs/liquibase/app/App.java
+++ src/main/java/com/rs/liquibase/app/App.java
@@ -1,13 +1,10 @@
 package com.rs.liquibase.app;
 
 /**
- * Hello world!
- *
+ * 
+ * @author ramesh
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+	}
 }
